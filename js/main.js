528b1b13cffba7f4d302277f0c8c9c171b056b9d
jliu4 <jliuapril@gmail.com>
2016-06-06 13:59:37 -0700

    two

--- js/main.js
+++ js/main.js
@@ -141,7 +141,7 @@ function getMediaSuccessCallback(stream) {
     localVideo.src =  window.URL.createObjectURL(stream);
     localVideo.srcObject = stream;
     localStream = stream;
-    if (localPC && localPC !=undefined ) localPC.addStream(stream);
+    //if (localPC && localPC !=undefined ) localPC.addStream(stream);
 }
 
 function getMediaErrorCallback(error){
@@ -221,7 +221,7 @@ function onLocalSessionCreated(desc) {
 
 function onRemoteStreamAdded(event) {
     
-      remoteVideo.src =  window.URL.createObjectURL(event.streams[0]);
+     // remoteVideo.src =  window.URL.createObjectURL(event.streams[0]);
       remoteVideo.srcObejct = event.streams[0];
       remoteVideo.play();
    
